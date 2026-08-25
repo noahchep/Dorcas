1e9417b1b9a4692f88dd49cd57fe7023e01faa8b
noahchep <noahchepkonga1@gmail.com>
2026-08-25 17:04:47 +0300

    Update portal-asisstant-ai.sql

--- Database/portal-asisstant-ai.sql
+++ Database/portal-asisstant-ai.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 25, 2026 at 04:01 PM
+-- Generation Time: Aug 25, 2026 at 04:04 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
